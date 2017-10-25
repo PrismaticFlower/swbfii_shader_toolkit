4efd6b6a84432597b9021d42df0399d649667125
SleepKiller <SleepKiller@users.noreply.github.com>
2017-10-26 01:08:29 +1300

    sorted compile.ps1

--- build/scripts/compile.ps1
+++ build/scripts/compile.ps1
@@ -1,35 +1,6 @@
 ﻿.\build\scripts\compile_helpers.ps1
 
 
-### Rain Shader ###
-
-compile_pass "rain" "rain_vs" "rain_ps"
-
-$shader = start_xml_shader "rain"   
-
-$shader += start_shader_state 0
-$shader += add_state_pass "rain" "rain_vs" "rain_ps"
-$shader += end_shader_state
-
-$shader += end_xml_shader
-
-$shader | Out-File -Encoding utf8 ".\build\xml\rain.xml"
-
-
-### Shadowquad Shader ###
-
-compile_pass "shadowquad" "shadowquad_vs" "shadowquad_ps"
-
-$shader = start_xml_shader "shadowquad"  
-
-$shader += start_shader_state 0
-$shader += add_state_pass "shadowquad" "shadowquad_vs" "shadowquad_ps"
-$shader += end_shader_state
-
-$shader += end_xml_shader
-
-$shader | Out-File -Encoding utf8 ".\build\xml\shadowquad.xml"
-
 ### Interface Shader ###
 
 compile_pass "interface" "masked_bitmap_vs" "masked_bitmap_ps"
@@ -58,3 +29,32 @@ $shader += end_shader_state
 $shader += end_xml_shader
 
 $shader | Out-File -Encoding utf8 ".\build\xml\interface.xml"
+
+### Rain Shader ###
+
+compile_pass "rain" "rain_vs" "rain_ps"
+
+$shader = start_xml_shader "rain"   
+
+$shader += start_shader_state 0
+$shader += add_state_pass "rain" "rain_vs" "rain_ps"
+$shader += end_shader_state
+
+$shader += end_xml_shader
+
+$shader | Out-File -Encoding utf8 ".\build\xml\rain.xml"
+
+
+### Shadowquad Shader ###
+
+compile_pass "shadowquad" "shadowquad_vs" "shadowquad_ps"
+
+$shader = start_xml_shader "shadowquad"  
+
+$shader += start_shader_state 0
+$shader += add_state_pass "shadowquad" "shadowquad_vs" "shadowquad_ps"
+$shader += end_shader_state
+
+$shader += end_xml_shader
+
+$shader | Out-File -Encoding utf8 ".\build\xml\shadowquad.xml"
